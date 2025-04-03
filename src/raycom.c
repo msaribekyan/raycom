0b7f06c49b8a02dae392339c9dcafc816f6c2036
Mher Saribekyan <saribekyanmher@gmail.com>
2025-04-03 10:41:05 +0400

    Minor changes

--- src/raycom.c
+++ src/raycom.c
@@ -1,43 +1,52 @@
 #include <stdint.h>
+#include <stdring.h>
 
 #include <raycom.h>
 #include <NRF24.h>
 
-#define MSG_TYPE_DISC 		0x0100
-#define MSG_TYPE_DISC_ACK 	0x0200
-#define MSG_TYPE_TABLE 		0x0300
-#define MSG_TYPE_PING 		0x0400
-#define MSG_TYPE_PING_ACK 	0x0500
-#define MSG_TYPE_CLEAR		0x0600
-#define MSG_TYPE_CLEAR_ACK	0x0700
+#define MSG_TYPE_DISC 		0x01
+#define MSG_TYPE_DISC_ACK 	0x02
+#define MSG_TYPE_TABLE 		0x03
+#define MSG_TYPE_PING 		0x04
+#define MSG_TYPE_PING_ACK 	0x05
+#define MSG_TYPE_CLEAR		0x06
+#define MSG_TYPE_CLEAR_ACK	0x07
+#define MSG_TYPE_MESSAGE	0x08
 
 // Message declarations
 typedef struct {
-	uint16_t message_conf;
-	uint8_t[30] message;
+	uint8_t[32] data;
 } message_t;
+
+message_t empty_message(){
+	message_t message;
+	memset(message.data, 0, 32);
+	return message;
+}
 
 // Queue setup
 typedef struct message_queue_node{
-	struct message_queue_node* next;
 	message_t message_packet;
+	struct message_queue_node* next;
 } message_queue_node;
 
 message_queue_node* message_queue_head = NULL;
+message_queue_node* message_queue_tail = NULL;
 
-uint8_t message_queue_node_push(message_t message){
+uint8_t message_enqueue(message_t message){
 	message_queue_node* new_message_queue_node;
 	new_message_queue_node = (message_queue_node*) malloc(sizeof(message_queue_node));
 
-	new_message_queue_node->next = message_queue_head;
-	new_message_queue_node->message = *message;
-
-	message_queue_head = new_message_queue_node;
+	new_message_queue_node->next = NULL;
+	new_message_queue_node->message = message;
+	
+	message_queue_tail->next = new_message_queue_node;
+	message_queue_tail = new_message_queue_node;
 
 	return 0;
 }
 
-uint8_t message_queue_node_pop(message_t* message){
+uint8_t message_dequeue(message_t* message){
 	if(message_queue_head == NULL){
 		return 1;
 	}
@@ -45,7 +54,11 @@ uint8_t message_queue_node_pop(message_t* message){
 	top_message_queue_node = message_queue_head;
 	message_queue_head = top_message_queue_node->next;
 
-	*message = message_queue_head->message;
+	if(message_queue_head->next == NULL){
+		message_queue_tail = NULL;
+	}
+
+	*message = top_message_queue_node->message;
 
 	free(top_message_queue_node);
 	return 0;
@@ -58,12 +71,18 @@ node_state_t node_state = STATE_IDLE;
 void nRF_init(uint8_t address){
 	node_address = address;
 }
+uint8_t RF_request_clearance(uint8_t dest, uint8_t channel){
+	message_t message = empty_message();
+	message.data[0] = MSG_TYPE_CLEAR;
+	message.data[1] = dest;
+	message.data[2] = node_address;
 
-void nRF_send(uint8_t* data, uint16_t size){
-	
+	message_queue_node_push(message);
+
+	return 0;
 }
 
-void nRF_common_queue(){
+void nRF_common(){
 	if(node_state == STATE_IDLE){
 		return;
 	}
